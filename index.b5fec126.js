b8ac73859ab82548d04642e42a0e926e5dcf94ac
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-13 14:23:15 +0200

    make build

---
+++ index.b5fec126.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.position="relative",t.style.position="absolute",t.style.top="50%",t.style.left="50%",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.b5fec126.js.map
